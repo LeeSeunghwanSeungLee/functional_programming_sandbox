d765a2eaa7a71a876282e5c6b9ebe700bc6107e5
lshn1007 <lshn1007@hanyang.ac.kr>
2022-08-31 19:24:26 +0900

    chore: _rest 까지 함수 설명 주석 추가

--- js/_.js
+++ js/_.js
@@ -23,8 +23,13 @@ function _curryr(fn) {
 var _get = _curryr(function(obj, key) {
   return obj == null ? undefined : obj[key];
 });
-  
 
+var _length = _get('length');
+
+/**
+ * list에 대해서 predi가 true가 되는 list만 필터링하는 함수
+ * @param {*} predi : filter를 시키고 싶은 boolean 리턴하는 함수
+ */
 function _filter(list, predi) {
   var new_list = [];
   _each(list, function(val) {
@@ -33,53 +38,65 @@ function _filter(list, predi) {
   return new_list;
 }
   
-  function _map(list, mapper) {
-    var new_list = [];
-    _each(list, function(val, key) {
-      new_list.push(mapper(val, key));
-    });
-    return new_list;
+/**
+ * mapper를 적용시켜서 새로운 value 들을 list에 담아서 리턴하는 함수
+ * @param {*} mapper : 함수로써, _each를 적용시킬 수 있는 함수를 의미한다
+ */
+function _map(list, mapper) {
+  var new_list = [];
+  _each(list, function(val, key) {
+    new_list.push(mapper(val, key));
+  });
+  return new_list;
+}
+
+/**
+ * object 타입인지 확인하는 함수
+ */
+function _is_object(obj) {
+  return typeof obj == 'object' && !!obj; // typeof null == 'object' 이기 때문에 !!obj를 적용한다.
+}
+  
+/**
+ * Object.keys라는 함수를 실행한다. 다만 _is_object 함수를 이용해 obj타입이 아니라면, 빈 배열을 리턴한다.
+ */
+function _keys(obj) {
+  return _is_object(obj) ? Object.keys(obj) : [];
+}
+  
+  
+/**
+ * @param {*} iter : 반복해서 적용하고 싶은 함수를 의미
+ */
+function _each(list, iter) {
+  var keys = _keys(list);
+  for (var i = 0, len = keys.length; i < len; i++) {
+    iter(list[keys[i]], keys[i]);
   }
+  return list;
+}
   
-  function _is_object(obj) {
-    return typeof obj == 'object' && !!obj; // typeof null == 'object' 이기 때문에 !!obj를 적용한다.
+var _map = _curryr(_map);
+  _each = _curryr(_each)
+  _filter = _curryr(_filter);
+
+var _pairs = _map(function (val, key) { return [key, val]; });
+
+var slice = Array.prototype.slice;
+function _rest(list, num) {
+  return slice.call(list, num || 1); // if !(num) return 1;
+}
+  
+function _reduce(list, iter, memo) {
+  if (arguments.length == 2) {
+    memo = list[0];
+    list = _rest(list);
   }
-  
-  function _keys(obj) {
-    return _is_object(obj) ? Object.keys(obj) : [];
-  }
-  
-  var _length = _get('length');
-  
-  function _each(list, iter) {
-    var keys = _keys(list);
-    for (var i = 0, len = keys.length; i < len; i++) {
-      iter(list[keys[i]], keys[i]);
-    }
-    return list;
-  }
-  
-  var _map = _curryr(_map),
-    _each = _curryr(_each),
-    _filter = _curryr(_filter);
-  
-  var _pairs = _map(function (val, key) { return [key, val]; });
-  
-  var slice = Array.prototype.slice;
-  function _rest(list, num) {
-    return slice.call(list, num || 1);
-  }
-  
-  function _reduce(list, iter, memo) {
-    if (arguments.length == 2) {
-      memo = list[0];
-      list = _rest(list);
-    }
-    _each(list, function(val) {
-      memo = iter(memo, val);
-    });
-    return memo;
-  }
+  _each(list, function(val) {
+    memo = iter(memo, val);
+  });
+  return memo;
+}
   
   function _pipe() {
     var fns = arguments;
